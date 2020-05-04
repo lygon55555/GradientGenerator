4fab7f53f386d347428cacd592c0dfc83f9e5cb7
김용현 <yonghyun.urssu@gmail.com>
2020-05-04 17:00:38 +0900

    Initial Commit

---
+++ GradientGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientGenerator
+//
+//  Created by Yonghyun on 2020/05/04.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
