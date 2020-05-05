aff33d88328c683228f2eaa4b1c91c8ceb7fd837
김용현 <yonghyun.urssu@gmail.com>
2020-05-05 12:34:21 +0900

    IBOutlet 추가

---
+++ GradientGenerator/GradientViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GradientViewController.swift
+//  GradientGenerator
+//
+//  Created by Yonghyun on 2020/05/05.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import UIKit
+
+class GradientViewController: UIViewController {
+    
+    @IBOutlet var gradientView: UIImageView!
+    @IBOutlet var colorImage1:  UIImageView!
+    @IBOutlet var colorImage2:  UIImageView!
+    @IBOutlet var colorImage3:  UIImageView!
+    
+    @IBOutlet var redSlider:   UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider:  UISlider!
+    @IBOutlet var alphaSlider: UISlider!
+    
+    @IBOutlet var redValueLabel:   UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel:  UILabel!
+    @IBOutlet var alphaValueLabel: UILabel!
+    
+    @IBOutlet var hexCodeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    
+    
+    
+}
